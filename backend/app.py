b8ddb64e0fe4715dcc05b53da4300214644addb5
bwoody13 <bwoody13@users.noreply.github.com>
2024-01-11 16:54:24 -0800

    update models and backend api to handle extra infor for syncing smart playlists

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,9 @@
+from datetime import datetime
 from flask import Flask, jsonify, request, session
 from flask_cors import CORS
 from dotenv import load_dotenv
 import os
-from models import db, User, Playlist, SmartPlaylist, PlaylistFollower
+from models import db, User, Playlist, SmartPlaylist, PlaylistFollower, Album, AlbumFollower
 from sqlalchemy import select
 
 
@@ -23,6 +24,7 @@ db.init_app(app)
 
 with app.app_context():
     db.create_all()
+    # db.drop_all()
 
 
 # ROUTES
@@ -45,17 +47,80 @@ def update_user():
         return jsonify({"error": "Access token is required to authenticate user"}), 401
 
     user = User.query.filter_by(id=id).first()
-    if not user:
-        user = User(id=id, name=data.get("name", ""))
-        db.session.add(user)
-    else:
-        user.name = data.get("name", "")
+    name = data.get("name")
+    if name:
+        if not user:
+            user = User(id=id, name=name)
+            db.session.add(user)
+        elif name != user.name:
+            user.name = name
 
     db.session.commit()
-    print("commit to DB")
     session['user_id'] = user.id
     session['access_token'] = access_token
     return jsonify({"message": "User updated successfully"}), 200
+
+
+@app.route("/albums", methods=["POST"])
+def update_albums():
+    user_id = session.get('user_id')
+    if not user_id:
+        return jsonify({"error": "User ID not yet in session"}), 401
+
+    access_token = session.get('access_token')
+    if not access_token:
+        return jsonify({"error": "Access token not yet in session"}), 401
+
+    data = request.json
+    for album in data.get("albums", []):
+        id = album.get("id")
+        name = album.get("name")
+        artistsData = album.get("artists")
+        if not artistsData:
+            return jsonify({"error": "Artists data is required"}), 400
+        artists = ", ".join([artist.get("name") for artist in artistsData])
+        if not id or not name or not artists:
+            return jsonify({"error": "Invalid album data"}), 400
+
+        genres = ", ".join(album.get("genres", ""))
+        images = album.get("images", [{}])
+        img_url = images[0].get("url", "") if images else ""
+
+        album = Album.query.filter_by(id=id).first()
+        if not album:
+            album = Album(id=id, name=name, artists=artists, genres=genres, img_url=img_url)
+            db.session.add(album)
+        elif name != album.name or artists != album.artists or genres != album.genres or img_url != album.img_url:
+            album.name = name
+            album.artists = artists
+            album.genres = genres
+            album.img_url = img_url
+
+        album_follower = AlbumFollower.query.filter_by(user_id=user_id, album_id=id).first()
+        if not album_follower:
+            album_follower = AlbumFollower(user_id=user_id, album_id=id)
+            db.session.add(album_follower)
+
+    user = User.query.filter_by(id=user_id).first_or_404()
+    user.album_sync_date = datetime.utcnow()
+
+    db.session.commit()
+    return jsonify({"message": "Albums synced successfully"}), 200
+
+
+@app.route("/albums", methods=["GET"])
+def get_albums():
+    user_id = session.get('user_id')
+    if not user_id:
+        return jsonify({"error": "User ID not yet in session"}), 401
+
+    access_token = session.get('access_token')
+    if not access_token:
+        return jsonify({"error": "Access token not yet in session"}), 401
+
+    albums = User.query.filter_by(id=user_id).first_or_404().liked_albums
+    return jsonify([Album.query.filter_by(id=album.album_id).first_or_404().to_dict()
+                    for album in albums]), 200
 
 
 @app.route("/playlists", methods=["POST"])
@@ -74,18 +139,30 @@ def update_playlists():
         name = playlist_data.get("name")
         owner_data = playlist_data.get("owner")
         owner_id = owner_data.get("id")
+        snapshot_id = playlist_data.get("snapshot_id")
+        if not playlist_id or not name or not owner_data or not owner_id or not snapshot_id:
+            return jsonify({"error": "Playlist data is incomplete"}), 400
+
+        owner_name = owner_data.get("name", "")
+        desc = playlist_data.get("description", "")
+        images = playlist_data.get("images", [{}])
+        img_url = images[0].get("url", "") if images else ""
+
         playlist = Playlist.query.filter_by(id=playlist_id, owner_id=owner_id).first()
         if not playlist:
             owner = User.query.filter_by(id=owner_id).first()
             if not owner:
-                owner_name = owner_data.get("name")
                 owner = User(id=owner_id, name=owner_name)
                 db.session.add(owner)
-            playlist = Playlist(id=playlist_id, name=name, owner_id=owner_id)
+            playlist = Playlist(id=playlist_id, name=name, owner_id=owner_id,
+                                snapshot_id=snapshot_id, desc=desc, img_url=img_url)
             db.session.add(playlist)
-        else:
+        elif snapshot_id != playlist.snapshot_id:
             playlist.name = name
             playlist.owner_id = owner_id
+            playlist.snapshot_id = snapshot_id
+            playlist.desc = desc
+            playlist.img_url = img_url
 
         playlist_follower = PlaylistFollower.query.filter_by(playlist_id=playlist_id,
                                                              user_id=user_id).first()
@@ -93,8 +170,27 @@ def update_playlists():
             playlist_follower = PlaylistFollower(playlist_id=playlist_id, user_id=user_id)
             db.session.add(playlist_follower)
 
+    user = User.query.filter_by(id=user_id).first()
+    user.playlist_sync_date = datetime.utcnow()
+
     db.session.commit()
     return jsonify({"message": "Playlists updated successfully"}), 200
+
+
+@app.route("/playlists", methods=["GET"])
+def get_playlists():
+    user_id = session.get('user_id')
+    if not user_id:
+        return jsonify({"error": "User ID not yet in session"}), 401
+
+    access_token = session.get('access_token')
+    if not access_token:
+        return jsonify({"error": "Access token not yet in session"}), 401
+
+    followed_playlists = User.query.filter_by(id=user_id).first_or_404().followed_playlists
+
+    return jsonify([Playlist.query.filter_by(id=playlist.playlist_id).first_or_404().to_dict()
+                    for playlist in followed_playlists]), 200
 
 
 @app.route("/smart_playlists", methods=["POST"])
@@ -155,12 +251,46 @@ def get_smart_playlists():
 
     owned_playlists = db.session.execute(stmt).scalars().all()
     smart_playlist_data = [{
-        "parent_playlist_id": op.id,
-        "children": [cp.child_playlist_id for cp in op.child_playlists]
+        "parent_playlist": op.to_dict(),
+        "children": [Playlist.query.filter_by(id=cp.child_playlist_id).first_or_404().to_dict()
+                     for cp in op.child_playlists]
     } for op in owned_playlists
         if op.child_playlists]
 
     return jsonify(smart_playlist_data), 200
+
+
+@app.route("/smart_playlists/sync", methods=["POST"])
+def sync_smart_playlists():
+    user_id = session.get('user_id')
+    if not user_id:
+        return jsonify({"error": "User ID not yet in session"}), 401
+
+    access_token = session.get('access_token')
+    if not access_token:
+        return jsonify({"error": "Access token not yet in session"}), 401
+
+    playlist_data = request.get_json()
+    if not playlist_data:
+        return jsonify({"error": "No playlist data provided"}), 400
+
+    parent_playlist_id = playlist_data.get("parent_playlist_id")
+    if not parent_playlist_id:
+        return jsonify({"error": "Parent playlist ID not provided"}), 400
+    children = playlist_data.get("children", [])
+    for child_playlist in children:
+        child_playlist_id = child_playlist.get("child_playlist_id")
+        snapshot_id = child_playlist.get("snapshot_id")
+        if not child_playlist_id or not snapshot_id:
+            return jsonify({"error": "Invalid child playlist data"}), 400
+
+        smart_playlist = SmartPlaylist.query.\
+            filter_by(parent_playlist_id=parent_playlist_id,
+                      child_playlist_id=child_playlist_id).first_or_404()
+        smart_playlist.last_sync_snapshot_id = snapshot_id
+
+    db.session.commit()
+    return jsonify({"message": "Smart playlist snapshots updated successfully"}), 200
 
 
 if __name__ == '__main__':
--- backend/models.py
+++ backend/models.py
@@ -1,36 +1,58 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.inspection import inspect
+from sqlalchemy.orm import ColumnProperty
+
 
 db = SQLAlchemy()
 
 
-class User(db.Model):
+class Base(db.Model):
+    __abstract__ = True
+
+    def to_dict(self):
+        data = {}
+        for c in inspect(self).mapper.column_attrs:
+            if isinstance(c, ColumnProperty):
+                data[c.key] = getattr(self, c.key)
+        return data
+            # print(c.key, getattr(self, c.key))
+        # return {c.name: getattr(self, c.name) for c in inspect(self).mapper.column_attrs}
+
+
+class User(Base):
     id = db.Column(db.String(), primary_key=True)
-    name = db.Column(db.String(), nullable=False)
+    name = db.Column(db.String(), nullable=True)
     album_sync_date = db.Column(db.DateTime, nullable=True)
     playlist_sync_date = db.Column(db.DateTime, nullable=True)
     owned_playlists = db.relationship('Playlist', backref='user', lazy=True,
                                       cascade="all, delete")
     followed_playlists = db.relationship('PlaylistFollower', backref='user', lazy=True,
                                          cascade="all, delete")
-    liked_albums = db.relationship('Album', backref='user', lazy=True,
+    liked_albums = db.relationship('AlbumFollower', backref='user', lazy=True,
                                    cascade="all, delete")
 
 
-class Album(db.Model):
+class Album(Base):
     id = db.Column(db.String(), primary_key=True)
     name = db.Column(db.String(), nullable=False)
+    artists = db.Column(db.String(), unique=False, nullable=False)
+    genres = db.Column(db.String(), unique=False, nullable=True)
+    img_url = db.Column(db.String(), unique=False, nullable=True)
 
 
-class AlbumFollower(db.Model):
+class AlbumFollower(Base):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.String(), db.ForeignKey('user.id'), nullable=False)
     album_id = db.Column(db.String(), db.ForeignKey('album.id'), nullable=False)
 
 
-class Playlist(db.Model):
+class Playlist(Base):
     id = db.Column(db.String(), primary_key=True)
     name = db.Column(db.String(), unique=False, nullable=False)
     owner_id = db.Column(db.String(), db.ForeignKey('user.id'), nullable=False)
+    snapshot_id = db.Column(db.String(), unique=False, nullable=False)
+    desc = db.Column(db.String(), unique=False, nullable=True)
+    img_url = db.Column(db.String(), unique=False, nullable=True)
     child_playlists = db.relationship('SmartPlaylist',
                                       foreign_keys="SmartPlaylist.parent_playlist_id",
                                       backref='playlist', lazy=True,
@@ -39,13 +61,14 @@ class Playlist(db.Model):
                                 cascade="all, delete")
 
 
-class PlaylistFollower(db.Model):
+class PlaylistFollower(Base):
     id = db.Column(db.Integer, primary_key=True)
     playlist_id = db.Column(db.String(), db.ForeignKey('playlist.id'), nullable=False)
     user_id = db.Column(db.String(), db.ForeignKey('user.id'), nullable=False)
 
 
-class SmartPlaylist(db.Model):
+class SmartPlaylist(Base):
     id = db.Column(db.Integer, primary_key=True)
     parent_playlist_id = db.Column(db.String(), db.ForeignKey('playlist.id'), nullable=False)
     child_playlist_id = db.Column(db.String(), db.ForeignKey('playlist.id'), nullable=False)
+    last_sync_snapshot_id = db.Column(db.String(), unique=False, nullable=True)
